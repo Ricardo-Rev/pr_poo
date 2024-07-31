247ae5f2a01609104138d63d3b0ae08bff99d564
Ricardo-Rev <rrevoloriop@miumg.edu.gt>
2024-07-30 18:02:14 -0600

    Primer commit

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author DELL
+ */
+public class Cliente extends Persona{
+    private String nit;
+    public Cliente(){}  
+    public Cliente(String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    
+    @Override
+    public String[] agregar() {
+        return new String[] {
+            getNit(),
+            getNombres(),
+            getApellidos(),
+            getDireccion(),
+            getTelefono(),
+            getFecha_nacimiento()
+        };
+    }
+}
